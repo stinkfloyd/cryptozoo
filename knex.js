3fa475f79a4e8b60fd474083e2b53678162ffd96
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-10-15 11:52:47 -0600

    "Basic Routes working"

--- knex.js
+++ knex.js
@@ -1,5 +1,5 @@
 // Require knex + detect environment
 const environment = process.env.NODE_ENV || 'development'
-const knex = require('knex')(knexConfig)
 const knexConfig = require('./knexfile')[environment]
+const knex = require('knex')(knexConfig)
 module.exports = knex
